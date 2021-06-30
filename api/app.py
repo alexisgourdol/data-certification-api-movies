2ccf793281873f0177ba45b8863b5a4f0cbd3ed7
Alexis Gourdol <alexis.gourdol@gmail.com>
2021-06-30 15:37:33 +0200

    added     CORSMiddleware

--- api/app.py
+++ api/app.py
@@ -6,6 +6,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 # define a root `/` endpoint
 @app.get("/")
